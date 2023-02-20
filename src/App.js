a543c57676d4375c865a3e79ccfbbea3c598a13a
TranVinh-ds <chenvinh2003@gmail.com>
2023-02-20 12:07:32 +0700

    create all feedback components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
+import { useState } from 'react';
+
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      const listItems = feedback.filter((item) => item.id !== id);
+      setFeedback(listItems);
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
