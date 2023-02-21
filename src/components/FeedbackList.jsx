12ccac51bf5ce82d1fe8182999e1f2a777533d6a
TranVinh-ds <chenvinh2003@gmail.com>
2023-02-21 11:12:27 +0700

    create json-server add feedback, set proxy and post method to json-server

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import { useContext } from 'react';
+import Spinner from './shared/Spinner';
 import { motion, AnimatePresence } from 'framer-motion';
 // import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 import FeedbackContext from '../context/FeedbackContext';
 
 const FeedbackList = () => {
-  const { feedback } = useContext(FeedbackContext);
-  if (!feedback || feedback.length === 0) {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
 
   return (
-    <div className="feedback-list">
-      <AnimatePresence>
-        {feedback.map((item) => (
-          <motion.div
-            key={item.id}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <FeedbackItem key={item.id} item={item} />
-          </motion.div>
-        ))}
-      </AnimatePresence>
-    </div>
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="feedback-list">
+          <AnimatePresence>
+            {feedback.map((item) => (
+              <motion.div
+                key={item.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <FeedbackItem key={item.id} item={item} />
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </div>
+      )}
+    </>
   );
 
   // return (
